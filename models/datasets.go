78a63918e3fbc7b4eac14b48ac5b68e554216943
Steffen Lundin <steffen.sandvold@gmail.com>
2018-06-07 22:14:18 +0200

    work in progress

---
+++ models/datasets.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	helper "github.com/quarkey/iot/json"
+)
+
+type Dataset struct {
+	ID          int              `db:"id" json:"id"`
+	SensorID    int              `db:"sensor_id" json:"sensor_id"`
+	Description string           `db:"description" json:"description"`
+	Reference   string           `db:"reference" json:"reference"`
+	IntervalSec int              `db:"intervalsec" json:"intervalsec"`
+	Fields      *json.RawMessage `db:"fields" json:"fields"`
+	CreatedAt   time.Time        `db:"created_at" json:"created_at"`
+}
+
+func (s *Server) Datasets(w http.ResponseWriter, r *http.Request) {
+	var datasets []Dataset
+	err := s.DB.Select(&datasets, "select id, sensor_id, description, reference, intervalsec, fields, created_at from dataset")
+	if err != nil {
+		log.Printf("unable to run query: %v", err)
+		helper.RespondHTTPErr(w, r, 500)
+		return
+	}
+	helper.Respond(w, r, 200, datasets)
+
+}
+func (s *Server) NewDataset(w http.ResponseWriter, r *http.Request) {
+	dat := Dataset{}
+	err := helper.DecodeBody(r, &dat)
+	if err != nil {
+		log.Printf("unable to decode body: %v", err)
+		helper.RespondHTTPErr(w, r, 500)
+		return
+	}
+	fmt.Println(dat.Fields)
+	_, err = s.DB.Exec("insert into dataset(sensor_id, description, reference, intervalsec, fields, created_at) values($1,$2,$3,$4,$5,$6)", dat.SensorID, dat.Description, dat.Reference, dat.IntervalSec, dat.Fields, dat.CreatedAt)
+	if err != nil {
+		log.Printf("unable to run query: %v", err)
+		helper.RespondHTTPErr(w, r, 500)
+		return
+	}
+	helper.RespondErr(w, r, http.StatusNotImplemented, "not implemented")
+}
