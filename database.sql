b463d4afb6faea99e755e9df673229a2c3b505b6
Steffen Lundin <steffen.sandvold@gmail.com>
2018-06-07 18:09:20 +0200

    setting up db structures for handling iot stuff

--- database.sql
+++ database.sql
@@ -1,26 +1,49 @@
--- sqlite:
-create table data (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    sensor_description text not null,
-    serial text not null,
-    temp text not null,
-    time text not null,
-);
-insert into data('sensor_description','serial','temp','time') values('sensor descr','serial no','23.12','2018-01-01 123301');
-
--- id  sensor_description  serial  temp
--- 1   arduino network xxx 15
--- {"sensor_description": "lala description","serial": "lala serial","temp": "23.11","time": "2018-0101 123301"}
- 
-
 -- postgres:
-create table sensordata(
+
+drop table sensordata;
+drop table dataset;
+drop table sensor;
+
+create table if not exists sensor (
+  id serial primary key,
+  description text not null,
+  key text not null,
+  created_at timestamp NOT NULL DEFAULT now()::timestamp(0)
+);
+create table if not exists dataset(
+  id serial primary key,
+  sensor_id integer references sensor (id),
+  description text not null,
+  reference text not null,
+  intervalsec int not null,
+  fields jsonb,
+  created_at timestamp NOT NULL DEFAULT now()::timestamp(0)
+);
+create table if not exists sensordata (
+  id serial primary key,
+  sensor_id integer references sensor (id),
+  dataset_id integer references dataset (id),
+  data jsonb,
+  time timestamp NOT NULL DEFAULT now()::timestamp(0)
+);
+
+insert into sensor(description, key) values('temp og hydro', 'arduino serial');
+insert into dataset(sensor_id, description, reference, intervalsec, fields) values(1,'temperatur measurement, growhouse','reference x',1800,'["temp", "hydro"]');
+insert into sensordata(sensor_id, dataset_id, data) values(1,currval(pg_get_serial_sequence('dataset', 'id')),'["23.13","59.32","ubro"]');
+
+select * from sensor;
+select * from dataset;
+select * from sensordata;
+
+
+
+--sqlite3
+create table sensor (
   id serial primary key,
   descr text not null,
   serial text not null,
   data jsonb not null
 );
-
 -- test data
 insert into sensordata(descr, serial, data) values('descr','serialno','{ "customer": "John Doe", "items": {"product": "Beer","qty": 6}}'::jsonb);
 insert into sensordata(descr, serial, data) values('descr 2','serialno 2','{"abc": "val"}'::jsonb);
