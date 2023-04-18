201b69a11e855d82739faa83d6ecf01ed495b5d1
Steffen Lundin <steffen.sandvold@gmail.com>
2023-04-18 21:37:52 +0200

    feat(frontend): Improving controller ui for mobile

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'iot-ng';
+  opened = true;
+  public innerWidth: any;
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+  }
+  public get windowBased() {
+    if (this.innerWidth > 768) {
+      return true;
+    }
+    return false;
+  }
 }
