9263df95c510b35f351303457fe528c516c62e30
Steffen Lundin <steffen.sandvold@gmail.com>
2023-07-10 22:28:26 +0200

    fix

--- client/src/app/views/events/events.component.ts
+++ client/src/app/views/events/events.component.ts
@@ -1,21 +1,18 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
-import { EventsList } from "src/app/models/event";
-import { EventsService } from "src/app/services/events.service";
-import { MatSort, Sort } from "@angular/material/sort";
-import { MatLegacyTableDataSource as MatTableDataSource } from "@angular/material/legacy-table";
-import { LiveAnnouncer } from "@angular/cdk/a11y";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { EventsList } from 'src/app/models/event';
+import { EventsService } from 'src/app/services/events.service';
+import { MatSort, Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
-  selector: "app-events",
-  templateUrl: "./events.component.html",
-  styleUrls: ["./events.component.scss"],
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent implements OnInit {
-  constructor(
-    private eventsService: EventsService,
-    private _liveAnnouncer: LiveAnnouncer
-  ) {}
-  displayedColumns: string[] = ["id", "category", "message", "event_time"];
+  constructor(private eventsService: EventsService, private _liveAnnouncer: LiveAnnouncer) {}
+  displayedColumns: string[] = ['id', 'category', 'message', 'event_time'];
   dataSource: any;
   loading: boolean = true;
   @ViewChild(MatSort) sort: MatSort;
